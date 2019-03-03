a48992bd40b12eb5e175b8f7b8ce310da8918bba
hbysdzl <382709807@qq.com>
2019-03-03 21:41:12 +0800

    网站配置完成

--- application/admin/controller/Config.php
+++ application/admin/controller/Config.php
@@ -16,7 +16,13 @@ class Config extends Common
      * @return \think\Response
      */
     public function index()
-    {
+    {   
+
+        //读取配置参数
+        $configdata = db('config')->find();
+        //dump($configdata);
+        $data = json_decode($configdata['config'],true);
+        $this->assign('data',$data);
         return $this->fetch();
     }
 
@@ -31,47 +37,48 @@ class Config extends Common
     public function save(Request $request)
     {
         $data = $request->post();
-        //dump($data);
 
         //判断库中是否存在记录无则添加有则修改
         $res = db('config')->find();
         //将配置信息转换为json字符串
         $config['config'] = json_encode($data,JSON_UNESCAPED_UNICODE);
-
         if(!$res){
             $result = db('config')->insert($config);
         }else{
             $result = db('config')->where('id',$res['id'])->update($config);
         }
 
-        if($result){
-            return json(array('code'=>'1','msgs'=>'设置成功！'));
-        }else{
+        if($result === false){
             return json(array('code'=>'0','msgs'=>'设置失败，请稍后重试！'));
+        }else{
+            return json(array('code'=>'1','msgs'=>'设置成功！'));
         }
 
     }
 
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
+    
 
     /**
-     * 显示编辑资源表单页.
+    *   
      *
-     * @param  int  $id
+     *  logo 异步上传
      * @return \think\Response
      */
-    public function edit($id)
+    public function uploadsLogo()
     {
-        //
+        $file = request()->file('file');
+        if ($file) {
+           $info = $file->move(ROOT_PATH.'public/static/','logo/logo');
+           if ($info) {
+               
+               //获取文件路径
+               return json($info->getSaveName());
+           }else{
+
+               //获取错误信息
+               return json($file->getError());
+           }
+        }
     }
 
     /**
